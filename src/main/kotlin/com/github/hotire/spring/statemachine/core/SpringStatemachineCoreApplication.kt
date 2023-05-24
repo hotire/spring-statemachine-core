db2eb5916da4714ebba806b44e77b12ae1d1f320
hotire <gngh0101@gmail.com>
2023-05-25 01:55:41 +0900

    init

---
+++ src/main/kotlin/com/github/hotire/spring/statemachine/core/SpringStatemachineCoreApplication.kt
@@ -0,0 +1,11 @@
+package com.github.hotire.spring.statemachine.core
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringStatemachineCoreApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringStatemachineCoreApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/hotire/spring/statemachine/core/SpringStatemachineCoreApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.github.hotire.spring.statemachine.core
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringStatemachineCoreApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
