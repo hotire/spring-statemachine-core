e8e6f7543a4322920796b635c2aac627026fb1ce
hotire <gngh0101@gmail.com>
2023-05-26 02:04:42 +0900

    init

--- src/main/kotlin/com/github/hotire/spring/statemachine/core/Application.kt
+++ src/main/kotlin/com/github/hotire/spring/statemachine/core/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SpringStatemachineCoreApplication
+class Application
 
 fun main(args: Array<String>) {
-    runApplication<SpringStatemachineCoreApplication>(*args)
+    runApplication<Application>(*args)
 }
