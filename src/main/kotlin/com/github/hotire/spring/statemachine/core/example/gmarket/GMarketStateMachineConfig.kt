93406fcb67618ab0e5941aa158f625a804c76515
hotire <gngh0101@gmail.com>
2023-06-07 01:12:50 +0900

    configure in StateMachine

--- src/main/kotlin/com/github/hotire/spring/statemachine/core/example/gmarket/GMarketStateMachineConfig.kt
+++ src/main/kotlin/com/github/hotire/spring/statemachine/core/example/gmarket/GMarketStateMachineConfig.kt
@@ -2,6 +2,21 @@ package com.github.hotire.spring.statemachine.core.example.gmarket
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter
+import org.springframework.statemachine.config.builders.StateMachineStateConfigurer
 
 @Configuration
-class GMarketStateMachineConfig : StateMachineConfigurerAdapter<States, Events>()
+class GMarketStateMachineConfig : StateMachineConfigurerAdapter<States, Events>() {
+
+    override fun configure(config: StateMachineStateConfigurer<States, Events>) {
+        config
+            .withStates()
+            .initial(States.S0)
+            .end(States.END)
+            .state(States.S5)
+            .state(States.S10)
+            .state(States.S15)
+            .state(States.S20)
+            .state(States.S25)
+            .state(States.S30)
+    }
+}
--- src/main/kotlin/com/github/hotire/spring/statemachine/core/example/gmarket/States.kt
+++ src/main/kotlin/com/github/hotire/spring/statemachine/core/example/gmarket/States.kt
@@ -1,7 +1,7 @@
 package com.github.hotire.spring.statemachine.core.example.gmarket
 
 enum class States {
-    S0, S5, S10, S15, S20, S25, S30
+    S0, S5, S10, S15, S20, S25, S30, END
 }
 
 enum class Events {
